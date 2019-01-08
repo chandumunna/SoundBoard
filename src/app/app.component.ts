e33e0d1d7ccf1baaa334fac3237010b4eaecbc77
Mattia La Spina <mattiaa95@gmail.com>
2019-01-08 19:57:49 +0000

    firts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,39 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  data: any;
+  webmedia: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
+  constructor() {
+    this.data = [
+			{ title:'ASSDESS', audio:'./assets/sounds/bird.mp3'},
+      { title:'¡Pajaro Loco!', audio:'./assets/sounds/bird.mp3'},
+      { title:'ASSadwDESS', audio:'./assets/sounds/bird.mp3'},
+      { title:'¡Pajarawdwao Loco!', audio:'./assets/sounds/bird.mp3'},
+      { title:'SS', audio:'./assets/sounds/bird.mp3'},
+      { title:'¡Pajawdaro Loco!', audio:'./assets/sounds/bird.mp3'},
+      { title:'ASSawdawdawdDESS', audio:'./assets/sounds/bird.mp3'},
+      { title:'¡Pajaro Loco!', audio:'./assets/sounds/bird.mp3'},
+      { title:'awd', audio:'./assets/sounds/bird.mp3'},
+      { title:'¡Pajarawdo Loco!', audio:'./assets/sounds/bird.mp3'},
+      { title:'ASSDawdESS', audio:'./assets/sounds/bird.mp3'},
+			{ title:'¡Pajadwadawwdawdawdaro adwwawedaLoco!', audio:'./assets/sounds/bird.mp3'},
+		];
+
+  }
+
+
+  play(soundFile){		
+      if(this.webmedia) {
+				this.webmedia.pause();
+			}
+			this.webmedia = new Audio(soundFile);
+			this.webmedia.load();
+			this.webmedia.play();
+
   }
 }
 
