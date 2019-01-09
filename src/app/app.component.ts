b3e4af44b91dc71bf74b8f32f3214fa94672b1b5
Mattia La Spina <mattiaa95@gmail.com>
2019-01-09 21:06:52 +0000

    sonidos terminadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,34 @@ export class MyApp {
 
   constructor() {
     this.data = [
-			{ title:'ASSDESS', audio:'./assets/sounds/bird.mp3'},
-      { title:'¡Pajaro Loco!', audio:'./assets/sounds/bird.mp3'},
-      { title:'ASSadwDESS', audio:'./assets/sounds/bird.mp3'},
-      { title:'¡Pajarawdwao Loco!', audio:'./assets/sounds/bird.mp3'},
-      { title:'SS', audio:'./assets/sounds/bird.mp3'},
-      { title:'¡Pajawdaro Loco!', audio:'./assets/sounds/bird.mp3'},
-      { title:'ASSawdawdawdDESS', audio:'./assets/sounds/bird.mp3'},
-      { title:'¡Pajaro Loco!', audio:'./assets/sounds/bird.mp3'},
-      { title:'awd', audio:'./assets/sounds/bird.mp3'},
-      { title:'¡Pajarawdo Loco!', audio:'./assets/sounds/bird.mp3'},
-      { title:'ASSDawdESS', audio:'./assets/sounds/bird.mp3'},
-			{ title:'¡Pajadwadawwdawdawdaro adwwawedaLoco!', audio:'./assets/sounds/bird.mp3'},
+      { title:'AAAHH', audio:'./assets/sounds/aaaaaaahh.mp3'},
+			{ title:'Aprobecha el Bug', audio:'./assets/sounds/Aprobechaelbug.mp3'},
+			{ title:'Aqui WillyRex Comentando', audio:'./assets/sounds/Aquiwillyrexcomentadno.mp3'},
+			{ title:'A ver, A ver...', audio:'./assets/sounds/averavertomasfalete.mp3'},
+			{ title:'HALA HALA HALA', audio:'./assets/sounds/HALAHALAHALA.mp3'},
+			{ title:'Ha Petao la Magia', audio:'./assets/sounds/hapetadolamagia.mp3'},
+			{ title:'Madre Mia mis Cosas', audio:'./assets/sounds/MadremiaMisCosas.mp3'},
+			{ title:'Madre Mia Willy', audio:'./assets/sounds/MadreMiaWilly.mp3'},
+			{ title:'Maldito juego de Cubos', audio:'./assets/sounds/Malditojuegodecubostio.mp3'},
+			{ title:'Me Cago en el Minecraft', audio:'./assets/sounds/Mecagoenelminecraft.mp3'},
+			{ title:'Me cago en Falete otra vez', audio:'./assets/sounds/Mecagoenfaleteotravez.mp3'},
+			{ title:'Me cago en todo lo cagable', audio:'./assets/sounds/Mecagoentodolocagablequeque.mp3'},
+			{ title:'Me estaba Viendo', audio:'./assets/sounds/Meestabaviendo.mp3'},
+			{ title:'No me lo creo tio', audio:'./assets/sounds/Nomelocreotio.mp3'},
+			{ title:'No me parece ni medio normal', audio:'./assets/sounds/Nomeparecenimedionormal.mp3'},
+			{ title:'NO NO NO NO', audio:'./assets/sounds/nonononhaha.mp3'},
+			{ title:'Ostras Julian', audio:'./assets/sounds/OstrasJulian.mp3'},
+			{ title:'Paradise Paradise', audio:'./assets/sounds/Paradise.mp3'},
+			{ title:'Pero Que Pero Que Dices', audio:'./assets/sounds/PeroQUePeroQueDices.mp3'},
+			{ title:'Por Favor', audio:'./assets/sounds/Porfavor.mp3'},
+			{ title:'Que leches ha pasado ahi', audio:'./assets/sounds/Quelecheshapasadoahi.mp3'},
+			{ title:'Que No Que No Que No Sal Sal', audio:'./assets/sounds/quenoquenoquenononosalsal.mp3'},
+			{ title:'Que Que ha pasado', audio:'./assets/sounds/QueQuehapasodo.mp3'},
+			{ title:'Que narices ha pasado ahi', audio:'./assets/sounds/Quenariceshapasadoahi.mp3'},
+			{ title:'Que timo Que timo', audio:'./assets/sounds/Quetimoquetimo.mp3'},
+			{ title:'Que no puedo salir', audio:'./assets/sounds/SQuenopuedosalir.mp3'},
+			{ title:'Tiene que areglarse tio', audio:'./assets/sounds/tienequeareglarsetio.mp3'},
+			{ title:'Tioooo', audio:'./assets/sounds/Tioooo.mp3'},
 		];
 
   }
@@ -37,3 +53,5 @@ export class MyApp {
   }
 }
 
+
+		
