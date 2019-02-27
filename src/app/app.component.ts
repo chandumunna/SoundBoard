34d63f0f0b07882fc058fbb1e87fb3532111fd19
Mattia La Spina <mattiaa95@gmail.com>
2019-02-27 19:40:29 +0000

    titot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,37 +16,23 @@ export class MyApp {
 	});
 
     this.data = [
-      { title:'AAAHH', audio:'./assets/sounds/aaaaaaahh.mp3'},
-			{ title:'Aprovecha el Bug', audio:'./assets/sounds/Aprobechaelbug.mp3'},
-			{ title:'Aqui WillyRex Comentando', audio:'./assets/sounds/Aquiwillyrexcomentadno.mp3'},
-			{ title:'A ver, a ver...', audio:'./assets/sounds/averavertomasfalete.mp3'},
-			{ title:'HALA HALA HALA', audio:'./assets/sounds/HALAHALAHALA.mp3'},
-			{ title:'Ha petao la Magia', audio:'./assets/sounds/hapetadolamagia.mp3'},
-			{ title:'Madre Mia mis Cosas', audio:'./assets/sounds/MadremiaMisCosas.mp3'},
-			{ title:'Madre Mia Willy', audio:'./assets/sounds/MadreMiaWilly.mp3'},
-			{ title:'Maldito juego de Cubos', audio:'./assets/sounds/Malditojuegodecubostio.mp3'},
-			{ title:'Me Cago en el Minecraft', audio:'./assets/sounds/Mecagoenelminecraft.mp3'},
-			{ title:'Me cago en Falete otra vez', audio:'./assets/sounds/Mecagoenfaleteotravez.mp3'},
-			{ title:'Me cago en todo lo cagable', audio:'./assets/sounds/Mecagoentodolocagablequeque.mp3'},
-			{ title:'Me estaba Viendo', audio:'./assets/sounds/Meestabaviendo.mp3'},
-			{ title:'No me lo creo tio', audio:'./assets/sounds/Nomelocreotio.mp3'},
-			{ title:'No me parece ni medio normal', audio:'./assets/sounds/Nomeparecenimedionormal.mp3'},
-			{ title:'NO NO NO NO', audio:'./assets/sounds/nonononhaha.mp3'},
-			{ title:'Ostras Julian', audio:'./assets/sounds/OstrasJulian.mp3'},
-			{ title:'Paradise Paradise', audio:'./assets/sounds/Paradise.mp3'},
-			{ title:'Pero Que Pero Que Dices', audio:'./assets/sounds/PeroQUePeroQueDices.mp3'},
-			{ title:'Por Favor', audio:'./assets/sounds/Porfavor.mp3'},
-			{ title:'Que leches ha pasado ahi', audio:'./assets/sounds/Quelecheshapasadoahi.mp3'},
-			{ title:'Que No Que No Que No Sal Sal', audio:'./assets/sounds/quenoquenoquenononosalsal.mp3'},
-			{ title:'Que Que ha pasado', audio:'./assets/sounds/QueQuehapasodo.mp3'},
-			{ title:'Que narices ha pasado ahi', audio:'./assets/sounds/Quenariceshapasadoahi.mp3'},
-			{ title:'Que timo Que timo', audio:'./assets/sounds/Quetimoquetimo.mp3'},
-			{ title:'Que no puedo salir', audio:'./assets/sounds/SQuenopuedosalir.mp3'},
-			{ title:'Tiene que areglarse tio', audio:'./assets/sounds/tienequeareglarsetio.mp3'},
-			{ title:'Tioooo', audio:'./assets/sounds/Tioooo.mp3'},
+      { title:'Cual quiera de nosotros puede ser Alcapone', audio:'./assets/sounds/Cualeuieradenosotrospuedeseralcapone.mp3'},
+			{ title:'Atento llego el momento', audio:'./assets/sounds/Atentollegoelmomento.mp3'},
+			{ title:'El mio pie el pio chi SevillaCity', audio:'./assets/sounds/Elmiopielpiochisevilla.mp3'},
+			{ title:'El que aprieta el gatillo te mata', audio:'./assets/sounds/Elqueaprietaelgatillotemata.mp3'},
+			{ title:'Soy como un caballo', audio:'./assets/sounds/SoyComounCaballo.mp3'},
+			{ title:'Estoy tan bueno que podria ser un prostituto', audio:'./assets/sounds/Estoytanbuenoquepodriaserunprostituto.mp3'},
+			{ title:'Putta', audio:'./assets/sounds/Putta.mp3'},
+			{ title:'Hablas mucho', audio:'./assets/sounds/HablasMucho.mp3'},
+			{ title:'Yeah', audio:'./assets/sounds/Yeah.mp3'},
+			{ title:'Yo contigo no discuto soy astuto', audio:'./assets/sounds/Yocontigonodiscutosoyastuto.mp3'},
+			{ title:'El que te Foka Putta', audio:'./assets/sounds/ElqueteFokaPutta.mp3'},
+			{ title:'El que no se pone nervioso', audio:'./assets/sounds/Elquenoseponenervioso.mp3'},
+			{ title:'Es la inteligencia', audio:'./assets/sounds/EsLaInteligencia.mp3'}
 		];
+
 		const addConfig: AdMobFreeBannerConfig = {
-			id: 'ca-app-pub-8119907816555669/7510366737',
+			id: 'ca-app-pub-8119907816555669/1683458437',
 			isTesting: false,
 			autoShow: true
 		   };
@@ -57,7 +43,6 @@ export class MyApp {
 			 .then(() => {})
 			 .catch(e => console.log(e));
   }
-
 
   play(soundFile){		
       if(this.webmedia) {
